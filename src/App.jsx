ea059d448abb09dd3f8a9718dd823b2a47212647
Jeet-bhardwaj <bhardwajjeet409@gmail.com>
2024-06-10 22:35:33 +0530

    Ron Swanson Quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
-function App() {
-  
-  return (
-    <>
-    <div className='hello'>hello</div>
-    <div className='hii'>hii</div>
-    </>
-  )
-}
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import QuoteCard from './QuoteCard';
+import SavedQuotes from './SavedQuotes';
+import './App.css';
 
-export default App
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [savedQuotes, setSavedQuotes] = useState([]);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+      setQuote(response.data[0]);
+    } catch (error) {
+      console.error('Error fetching the quote', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const saveQuote = () => {
+    setSavedQuotes([...savedQuotes, quote]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Ron Swanson Quotes</h1>
+      <QuoteCard quote={quote} fetchQuote={fetchQuote} saveQuote={saveQuote} />
+      <SavedQuotes savedQuotes={savedQuotes} />
+    </div>
+  );
+};
+
+export default App;
